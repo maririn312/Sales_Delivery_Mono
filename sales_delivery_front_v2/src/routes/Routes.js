7232deff0ff0a2785bc89197bfa8f7af69ba0db3
Marin <khatanzorigb@gmail.com>
2023-12-20 00:31:41 +0800

    master changes

---
+++ sales_delivery_front_v2/src/routes/Routes.js
@@ -0,0 +1,41 @@
+import LoginPage from "../pages/Login.jsx";
+import SignupPage from "../pages/SignupPage.jsx";
+import ActivationPage from "../pages/ActivationPage.jsx";
+import HomePage from "../pages/HomePage.jsx";
+import ProductsPage from "../pages/ProductsPage.jsx";
+import BestSellingPage from "../pages/BestSellingPage.jsx";
+import EventsPage from "../pages/EventsPage.jsx";
+import FAQPage from "../pages/FAQPage.jsx";
+import CheckoutPage from "../pages/CheckoutPage.jsx";
+import PaymentPage from "../pages/PaymentPage.jsx";
+import OrderSuccessPage from "../pages/OrderSuccessPage.jsx";
+import ProductDetailsPage from "../pages/ProductDetailsPage.jsx";
+import ProfilePage from "../pages/ProfilePage.jsx";
+import ShopCreatePage from "../pages/ShopCreate.jsx";
+import SellerActivationPage from "../pages/SellerActivationPage.jsx";
+import ShopLoginPage from "../pages/ShopLoginPage.jsx";
+import OrderDetailsPage from "../pages/OrderDetailsPage.jsx";
+import TrackOrderPage from "../pages/TrackOrderPage.jsx";
+import UserInbox from "../pages/UserInbox.jsx";
+
+export { 
+    LoginPage,
+    SignupPage,
+    ActivationPage,
+    HomePage,
+    ProductsPage,
+    BestSellingPage,
+    EventsPage,
+    FAQPage,
+    CheckoutPage,
+    PaymentPage,
+    OrderSuccessPage,
+    ProductDetailsPage,
+    ProfilePage,
+    ShopCreatePage,
+    SellerActivationPage,
+    ShopLoginPage,
+    OrderDetailsPage,
+    TrackOrderPage,
+    UserInbox
+};
