09d240f059819e3a4c6c9aa951a347d056bde6f9
Marin <khatanzorigb@gmail.com>
2024-01-04 01:33:23 +0800

    mobile update

--- sales_delivery_mobile/lib/webviewstack.dart
+++ sales_delivery_mobile/lib/webviewstack.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewStack extends StatefulWidget {
@@ -15,38 +16,59 @@ class _WebViewStackState extends State<WebViewStack> {
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(NavigationDelegate(
-        onPageStarted: (url) {
-          if (mounted) {
-            setState(() {
-              loadingPercentage = 0;
-            });
-          }
-        },
-        onProgress: (progress) {
-          setState(() {
-            loadingPercentage = progress;
-          });
-        },
-        onPageFinished: (url) {
-          setState(() {
-            loadingPercentage = 100;
-          });
-        },
-      ))
-      ..loadRequest(
-        Uri.parse('https://sales-delivery-mono-9264.vercel.app'),
-      );
+    // controller = WebViewController()
+    //   ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    //   ..setNavigationDelegate(NavigationDelegate(
+    //     onPageStarted: (url) {
+    //       if (mounted) {
+    //         setState(() {
+    //           loadingPercentage = 0;
+    //         });
+    //       }
+    //     },
+    //     onProgress: (progress) {
+    //       setState(() {
+    //         loadingPercentage = progress;
+    //       });
+    //     },
+    //     onPageFinished: (url) {
+    //       setState(() {
+    //         loadingPercentage = 100;
+    //       });
+    //     },
+    //   ))
+    //   ..loadRequest(
+    //     Uri.parse('https://sales-delivery-mono-9264.vercel.app'),
+    //   );
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        WebViewWidget(
-          controller: controller,
+        InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri("https://sales-delivery-mono-9264.vercel.app"),
+          ),
+          // androidOptions: AndroidInAppWebViewOptions(
+          //   useWideViewPort: true,
+          //   loadWithOverviewMode: true,
+          //   databaseEnabled: true,
+          //   displayZoomControls: true,
+          //   builtInZoomControls: true,
+          //   thirdPartyCookiesEnabled: true,
+          //   userAgent: "My Custom User Agent",
+          //   // Add more options as needed
+          // ),
+          onWebViewCreated: (InAppWebViewController controller) {
+            // WebView has been created
+          },
+          // onLoadStart: (InAppWebViewController controller, Uri url) {
+          //   // WebView has started loading a new page
+          // },
+          // onLoadStop: (InAppWebViewController controller, Uri url) {
+          //   // WebView has finished loading a page
+          // },
         ),
         if (loadingPercentage < 100)
           LinearProgressIndicator(
